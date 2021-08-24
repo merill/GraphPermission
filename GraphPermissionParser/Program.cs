335441993d8a9fc982a49265d03dd5fcdb3c540a
Merill Fernando <merill@merill.net>
2021-08-24 21:55:37 +1000

    Added clean up and ignore code

--- GraphPermissionParser/Program.cs
+++ GraphPermissionParser/Program.cs
@@ -1,9 +1,12 @@
-﻿using Markdig;
+﻿using CsvHelper;
+using Markdig;
 using Markdig.Extensions.Tables;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
@@ -16,11 +19,19 @@ namespace GraphPermissionParser
         {
             List<GraphPermission> permissions = new List<GraphPermission>();
             var folder = @"F:\code\microsoft-graph-docs\api-reference\v1.0\api\";
-            foreach(var filePath in Directory.GetFiles(folder))
+            foreach (var filePath in Directory.GetFiles(folder))
             {
                 permissions.AddRange(GetPermissions(filePath));
             }
-            //doc.IndexOf(new Markdig.Syntax.HeadingBlock() { Inline = new Markdig.Syntax.Inlines.ContainerInline() { } })
+
+            //permissions.AddRange(GetPermissions(@"F:\code\microsoft-graph-docs\api-reference\v1.0\api\intune-rbac-deviceandappmanagementroledefinition-delete.md"));
+
+            using (var writer = new StreamWriter(@"F:\code\temp\graphperm.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(permissions);
+            }
+
             Console.WriteLine("finidhe reading");
         }
 
@@ -38,7 +49,7 @@ namespace GraphPermissionParser
 
             var permissions = new List<GraphPermission>();
             var codeBlock = doc.Descendants<FencedCodeBlock>().First();
-            var httpRequest = codeBlock.Lines.ToString();
+            var httpRequests = codeBlock.Lines.ToString().Split(Environment.NewLine);
 
             foreach (var table in permGraphTable)
             {
@@ -49,18 +60,47 @@ namespace GraphPermissionParser
                         var cells = row.Descendants<TableCell>().ToList();
                         var permissionType = cells[0].Descendants<LiteralInline>().FirstOrDefault().Content.ToString();
                         var permission = cells[1].Descendants<LiteralInline>().FirstOrDefault().Content.ToString();
-                        permissions.Add(new GraphPermission()
+                        foreach(var perm in permission.Split(','))
                         {
-                            PermissionType = permissionType,
-                            Permission = permission,
-                            HttpRequest = httpRequest,
-                            SourceFile = filePath
-                        });
+                            if(!string.IsNullOrWhiteSpace(perm))
+                            {
+                                if (IsValidPermission(perm))
+                                {
+                                    foreach (var httpRequest in httpRequests)
+                                    {
+                                        if (!string.IsNullOrWhiteSpace(httpRequest))
+                                        {
+                                            permissions.Add(new GraphPermission()
+                                            {
+                                                PermissionType = permissionType,
+                                                Permission = perm.Trim(),
+                                                HttpRequest = httpRequest,
+                                                SourceFile = filePath
+                                            });
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
 
+            if(permissions.Count ==0)
+            {
+                Console.WriteLine("Warning: No permissions found on {0}", filePath);
+            }
             return permissions;
+        }
+
+        private static bool IsValidPermission(string perm)
+        {
+            var ignorePerms = new string[] { "not supported", "not applicable", "none", "n/a" };
+            foreach(var ignore in ignorePerms)
+            {
+                if (perm.Contains(ignore, StringComparison.InvariantCultureIgnoreCase)) { return false; }
+            }
+            return true;
         }
     }
 }
