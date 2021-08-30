30a16ad2258f9f2efc964bc0a4331ec52d43c256
Merill Fernando <merill@merill.net>
2021-08-30 13:07:14 +1000

    Added params

--- GraphPermissionParser/Program.cs
+++ GraphPermissionParser/Program.cs
@@ -12,8 +12,9 @@ namespace GraphPermissionParser
         static async Task Main(string[] args)
         {
             var parser = new GraphDocParser();
-            var folder = @"F:\code\microsoft-graph-docs\api-reference\v1.0\api\";
-            var permissions = parser.GetPermissionsInFolder(folder);
+            var apiFolder = @"F:\code\microsoft-graph-docs\api-reference\v1.0\api\";
+            apiFolder = args[0];
+            var permissions = parser.GetPermissionsInFolder(apiFolder);
 
             //var filePath = @"F:\code\microsoft-graph-docs\api-reference\v1.0\api\intune-rbac-deviceandappmanagementroledefinition-delete.md";
             //var permissions = parser.GetPermissionsInFile(filePath);
@@ -21,9 +22,10 @@ namespace GraphPermissionParser
             var config = GetConfig();
             var mdg = new MarkdownGenerator(config);
             var mdFolder = @"F:\Code\temp\graphpermdoc\docfx_project";
+            mdFolder = args[1];
             var result = await mdg.GenerateAsync(permissions, mdFolder);
-            var csvFilePath = @"F:\code\temp\graphperm.csv";
-            Csv.SavePermissionsToCsv(permissions, csvFilePath);
+            //var csvFilePath = @"F:\code\temp\graphperm.csv";
+            //Csv.SavePermissionsToCsv(permissions, csvFilePath);
 
         }
 
